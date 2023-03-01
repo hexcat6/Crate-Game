08dbf885a6086c32beaddb2a72b13667cdbfdda1
sherbitfish <81139949+sherbitfish@users.noreply.github.com>
2023-03-01 21:30:14 +1030

    spawn crates

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <cstring>
-
+#include <time.h> 
 
 using namespace std;
 
@@ -22,26 +22,78 @@ using namespace std;
 
 class crate {
     public:
-        string colour;
+        int colour;
         int column;
         int row;
-        bool infectived;
+        bool freespace;
+
         void draw() {
-            //code
-            setcolour(colour);
-            mvprintw(19,7*column+5,"1");
+            attrset(COLOR_PAIR(colour));
+            mvprintw(-2*row+15,7*column+4,"┏━┓");
+            mvprintw(-2*row+16,7*column+4,"┗━┛");
+        }
+};
+class crate coordinates[7][6];//7 column, 6 rows 
+
+class inventory {
+    public:
+
+        void spawn() {
+            // srand (time(0));
+            int randomcrate = rand() % 101;
+            int randomcoliumn = rand() % 7;
+            string craterarity[100] = {"white", "pink", "pink", "red", "red", "red", "yellow", "yellow", "yellow", "yellow", "green", "green", "green", "green", "green", "green", "green", "green", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing"};
+
+            if (!(craterarity[randomcrate] == "nothing")) {
+                for (int i = 0; i <= 6; i++) {
+                    if (coordinates[randomcoliumn][i].freespace == true) {
+                        crate newcrate;
+                        if (craterarity[randomcrate] == "blue") {
+                            newcrate.colour = 1;//blue
+                        } else if (craterarity[randomcrate] == "cyan") {
+                            newcrate.colour = 2;//cyan
+                        } else if (craterarity[randomcrate] == "green") {
+                            newcrate.colour = 3;//green
+                        } else if (craterarity[randomcrate] == "yellow") {
+                            newcrate.colour = 4;//yellow
+                        } else if (craterarity[randomcrate] == "red") {
+                            newcrate.colour = 5;//red
+                        } else if (craterarity[randomcrate] == "pink") {
+                            newcrate.colour = 6;//pink
+                        } else if (craterarity[randomcrate] == "white") {
+                            newcrate.colour = 7;//white
+                        }
+                        newcrate.column = randomcoliumn;
+                        newcrate.row = i;
+                        newcrate.freespace = false;
+                        coordinates[randomcoliumn][i] = newcrate;
+                        break;
+                    }
+                }
+            }
+        }
+
+        void draw () {
+            for (int c = 0; c <= 7; c++) {
+                for (int r = 0; r <= 6; r++) {
+                    if (coordinates[c][r].colour != 0) {
+                        coordinates[c][r].draw();
+                    }
+                }
+            }
         }
 };
 
 class player {
 
-    public:
-        int column;
+    private:
         int x;
-        int row;
-        int button;
         int arm;
+        int maxarm;
+    public:
+        int button;
         string status; // standby, grabbing, placeing, lowering, raising
+        class crate holding;
 
 
         void move() {
@@ -57,8 +109,18 @@ class player {
             }
 
             if (button == x) {
-                column = button;
-                if (arm < 15) {
+                for (int y = 0; y <= 6; y++) {
+                    if (coordinates[x][y].freespace == true) {
+                        maxarm = -2*y+15;
+                        break;
+                    }
+                    if ((coordinates[x][y].freespace == false) && (y == 6)) {
+                        maxarm = 3;
+                        break;
+                    }
+                }
+                // maxarm = -2*i+15;
+                if (arm < maxarm) {
                     arm++;
                 }
                 if (status == "standby") {
@@ -68,7 +130,6 @@ class player {
         }
 
         void draw () {
-            init_pair(7, COLOR_WHITE, -1);
             attrset(COLOR_PAIR(7));
             mvprintw(0,7*x+5,"┓");
             if (arm) {
@@ -81,124 +142,18 @@ class player {
 
 };
 
-class inventory {
-    public:
-        //7 column, 6 rows 
-        class crate coordinates[7][6];
-        void spawn() {
-            
-            int randomcrate = 1+ (rand() % 100);
-            int randomcoliumn = 0+ (rand() % 6);
-
-            if (randomcrate == 1) {
-                //white crate
-                crate newcrate;
-                newcrate.colour = 1;
-                newcrate.column = randomcoliumn;
-                coordinates[randomcoliumn][1] = newcrate;
-            }
-            if (2 >= randomcrate && randomcrate >= 3) {
-                //pink crate
-                crate newcrate;
-                newcrate.colour = "pink";
-            }
-            if (4 >= randomcrate && randomcrate >= 8) {
-                //red crate
-                crate newcrate;
-                newcrate.colour = "red";
-            }
-            if (9 >= randomcrate && randomcrate >= 15) {
-                //yellow crate
-                crate newcrate;
-                newcrate.colour = "yellow";
-            }
-            if (16 >= randomcrate && randomcrate >= 24) {
-                //green crate
-                crate newcrate;
-                newcrate.colour = "green";
-            }
-            if (25 >= randomcrate && randomcrate >= 35) {
-                //cyan crate
-                crate newcrate;
-                newcrate.colour = "cyan";
-            }
-            if (36 >= randomcrate && randomcrate >= 48) {
-                //blue crate
-                crate newcrate;
-                newcrate.colour = "blue";
-            }
-            if (49 >= randomcrate && randomcrate >= 59) {
-                //tnt crate
-                crate newcrate;
-                newcrate.colour = "tnt";
-            }
-            
-            if (60 >= randomcrate && randomcrate >= 100) {
-                //nothing
-            }
-        }
-
-        void draw () {
-            for (int c = 1; c <= 7; c++) {
-                for (int r = 1; r <= 6; r++) {
-                    coordinates[c][r].draw();
-                }
-            }
-        }
-};
 
 int ch;
 int currentscore = 0;
 bool gameover = false;
-//string craterarity[100] = {'white', 'pink', 'pink', 'red', 'red', 'red', 'yellow', 'yellow', 'yellow', 'yellow', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'cyan', 'cyan', 'cyan', 'cyan', 'cyan', 'cyan', 'cyan', 'cyan', 'cyan', 'cyan', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'nothing', 'tnt', 'tnt', 'tnt', 'tnt', 'tnt', 'tnt', 'tnt', 'tnt', 'tnt', 'tnt'};
 
-player player;
+
 inventory inventory;
-
+player player;
 // different colours
 //COLOR_YELLOW, COLOR_CYAN, COLOR_BLUE, COLOR_WHITE, COLOR_RED, COLOR_GREEN, COLOR_MAGENTA
 
 //functions
-
-void setcolour (string colour) {
-    if (colour == "white") {
-        init_pair(1, COLOR_WHITE, -1);
-        attrset(COLOR_PAIR(1));
-    }
-    if (colour == "pink") {
-        init_pair(1, COLOR_MAGENTA, -1);
-        attrset(COLOR_PAIR(1));
-    }
-    if (colour == "red") {
-        init_pair(1, COLOR_RED, -1);
-        attrset(COLOR_PAIR(1));
-    }
-    if (colour == "yellow") {
-        init_pair(1, COLOR_YELLOW, -1);
-        attrset(COLOR_PAIR(1));
-    }
-    if (colour == "green") {
-        init_pair(1, COLOR_GREEN, -1);
-        attrset(COLOR_PAIR(1));
-    }
-    if (colour == "cyan") {
-        init_pair(1, COLOR_CYAN, -1);
-        attrset(COLOR_PAIR(1));
-    }
-    if (colour == "blue") {
-        init_pair(1, COLOR_BLUE, -1);
-        attrset(COLOR_PAIR(1));
-    }
-    if (colour == "black") {
-        init_pair(1, COLOR_BLACK, -1);
-        attrset(COLOR_PAIR(1));
-    }
-    if (colour == "tnt") {
-        init_pair(1, COLOR_RED, COLOR_WHITE);
-        attrset(COLOR_PAIR(1));
-    }
-}
-
 string readLine(string str, int n) {
     // returns the nth line of a string
     stringstream f(str);
@@ -219,101 +174,80 @@ void draw() {
     for (int i = 0; i < 18; i++) {
         mvprintw(i,0,readLine(screenstring, i).c_str());
     }
-    player.draw();
     inventory.draw();
-    init_pair(1, COLOR_BLUE, -1);
+    player.draw();
+
+
     attrset(COLOR_PAIR(1));
 
     mvprintw(10,55,"┏━┓");
     mvprintw(11,55,"┗━┛");
 
-    init_pair(2, COLOR_CYAN, -1);
     attrset(COLOR_PAIR(2));
 
     mvprintw(10,60,"┏━┓");
     mvprintw(11,60,"┗━┛");
 
-    init_pair(3, COLOR_GREEN, -1);
     attrset(COLOR_PAIR(3));
 
     mvprintw(10,65,"┏━┓");
     mvprintw(11,65,"┗━┛");
 
-    init_pair(4, COLOR_YELLOW, -1);
     attrset(COLOR_PAIR(4));
 
     mvprintw(10,70,"┏━┓");
     mvprintw(11,70,"┗━┛");
 
-    init_pair(5, COLOR_RED, -1);
     attrset(COLOR_PAIR(5));
 
     mvprintw(10,75,"┏━┓");
     mvprintw(11,75,"┗━┛");
 
-    init_pair(6, COLOR_MAGENTA, -1);
     attrset(COLOR_PAIR(6));
 
     mvprintw(10,80,"┏━┓");
     mvprintw(11,80,"┗━┛");
 
-    init_pair(7, COLOR_WHITE, -1);
     attrset(COLOR_PAIR(7));
 
     mvprintw(10,85,"┏━┓");
     mvprintw(11,85,"┗━┛");
 
-    init_pair(8, COLOR_BLACK, -1);
     attrset(COLOR_PAIR(8));
 
     mvprintw(10,90,"┏━┓");
     mvprintw(11,90,"┗━┛");
 
-
-    init_pair(7, COLOR_WHITE, -1);
     attrset(COLOR_PAIR(7));
 }
 
 void game() {
 	switch(ch) {
-        case '1':     
-            mvprintw(12,60,"1");
-            player.column = 0;
+        case '1':
             player.button = 0;
             break;
         case '2': 
-            mvprintw(12,60,"2");
-            player.column = 1;
             player.button = 1;
             break;
         case '3': 
-            mvprintw(12,60,"3"); 
-            player.column = 2;
             player.button = 2;
             break;
         case '4':
-            mvprintw(12,60,"4");
-            player.column = 3;
             player.button = 3;
             break;
         case '5':
-            mvprintw(12,60,"5");
-            player.column = 4;
             player.button = 4;
             break;
         case '6':
-            mvprintw(12,60,"6");
-            player.column = 5;
             player.button = 5;
             break;
         case '7':
-            mvprintw(12,60,"7");
-            player.column = 6;
             player.button = 6;
             break;
         default: 
             break;
     }
+
     player.move();
     inventory.spawn();
 }
@@ -331,9 +265,25 @@ int main()
     curs_set(0);
 	start_color();
     use_default_colors();
+
+    init_pair(1, COLOR_BLUE, -1);
+    init_pair(2, COLOR_CYAN, -1);
+    init_pair(3, COLOR_GREEN, -1);
+    init_pair(4, COLOR_YELLOW, -1);
+    init_pair(5, COLOR_RED, -1);
+    init_pair(6, COLOR_MAGENTA, -1);
+    init_pair(7, COLOR_WHITE, -1);
+    init_pair(8, COLOR_BLACK, -1);
+
     player.status = "standby";
-    // player.status = "grabbing";
-    
+    player.status = "grabbing";
+    for (int y = 0; y < 7; y++) {
+        for (int x = 0; x < 6; x++) {
+            coordinates[y][x].freespace = true;
+        }
+    }
+
+
     //clear the screen and refreash it
     clear();
     refresh();
@@ -347,8 +297,6 @@ int main()
     }
     //and ends here
     refresh();
-
-    // while(1);
     //exit the game
     endwin();
     return 0;

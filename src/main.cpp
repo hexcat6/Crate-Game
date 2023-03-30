e44e41ad9e29e4de955b0d2d218822732f146e04
sherbitfish <81139949+sherbitfish@users.noreply.github.com>
2023-03-30 15:20:26 +1030

    bug fixes

--- src/main.cpp
+++ src/main.cpp
@@ -33,35 +33,34 @@ class crate {
         }
 };
 
-class Inventory {
-    private:
-        int top[7];
+class inventory {
     public:
+        int top[7];
         class crate coordinates[7][6];
-        Inventory() {
-            for (int i = 0; i < 7; i++) {
-                top[i] = -1;
-            }
-        }
+
         bool push(int x, class crate item) {
-            if (top[x] > 7) {
-                // cout << "Stack Overflow!!!";
+            if (top[x] > 6) {//full
                 return false;
             } else {
                 coordinates[x][top[x]++] = item;
-                // cout<<item<<endl;
                 return true;
             }
         };
+
         class crate pop(int x) {
-            if (!(top[x] < 0)) {
-                class crate item = coordinates[x][top[x]--];
-                return item;
-            }
+            class crate item = coordinates[x][top[x]--];
+            coordinates[x][top[x]].freespace = true;
+            return item;
         };
+
         bool isEmpty(int x) {
-            return (top[x] < 0);
+            return (top[x] == 0);
         };
+
+        bool isFull(int x) {
+            return (top[x] == 6);
+        };
+
         void draw() {
             for (int x = 0; x < 7; x++) {
                 for (int y = 0; y < 6; y++) {
@@ -71,13 +70,15 @@ class Inventory {
                 }
             }
         };
-        void spawn() {
+
+        void spawn(int playerx) {
             // srand (time(0));
             int randomcrate = rand() % 100;
             int randomx = rand() % 7;
             string craterarity[100] = {"white", "pink", "pink", "red", "red", "red", "yellow", "yellow", "yellow", "yellow", "green", "green", "green", "green", "green", "green", "green", "green", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing"};
-            if (!(craterarity[randomcrate] == "nothing") && !(player.x == randomx)) {
-                int y = top[x];
+            if (!(craterarity[randomcrate] == "nothing") && !(playerx == randomx)) {
+                if (!(isFull(randomx))) {
+                int y = top[randomx];
                     if (coordinates[randomx][y].freespace == true) {
                         class crate newcrate;
                         if (craterarity[randomcrate] == "blue") {
@@ -99,17 +100,13 @@ class Inventory {
                         newcrate.row = y;
                         newcrate.freespace = false;
                         push(randomx, newcrate);
-                        // coordinates[randomx][y] = newcrate;
-                        // columnsize[randomx]++;
-                        break;
                     }
-                
+                }
             }
         }
+        
 };
-
-class crate coordinates[7][6];//7 column, 6 rows 
-int columnsize[7];
+class inventory inventory;
 
 class player {
 
@@ -120,21 +117,21 @@ class player {
     public:
         int x;
         int button;
-             // standby, grabbing, placeing, lowering, raising
+        int onclick;
+        // standby, grabbing, placeing, lowering, raising
         enum status {
             standby,
             raising,
             lowering
         };
-        status status;
+        enum status status;
         class crate holding;
+        bool isHolding;
 
 
         void move() {
 
-            if (button != x || status == standby) {
-                status = standby;
-
+            if (button != x) {
                 if (arm > 0) {
                     arm--;
                 } else if (button < x) {
@@ -143,56 +140,51 @@ class player {
                     x++;
                 }
                 
+                if (button == x) {
+                    status = lowering;
+                } else if (button != x) {
+                    status = standby;
+                }
+
             }
 
-            if (button == x) {
-                if (status != raising) {
+            if (onclick == x || button == x) {
+                y = inventory.top[x];
+                if (isHolding == false) {// work out how low the arm can go when holding nothing
+                    if (inventory.isFull(x)) {// column is full and arm is holding nothing
+                        maxarm = 3;
+                    } else if (inventory.isEmpty(x)) {// column is empty and arm is holding nothing
+                        maxarm = 15;
+                    } else { // is holding nothing
+                        maxarm = -2*y+16;
+                    }
+                } else if (isHolding == true) {// work out how low the arm can go when holding a crate
+                    if (inventory.isFull(x)) {// column is full and arm is holding a crate
+                        maxarm = 0;
+                    } else if (inventory.isEmpty(x)) {// column is empty and arm is holding a crate
+                        maxarm = 13;
+                    } else { // is holding a crate
+                        maxarm = -2*y+13;
+                    }
+                }
+
+                if (arm < maxarm) { //lower and raise arm animation
                     status = lowering;
                 }
-                y = columnsize[x];
-                if (holding.freespace) {// not holding anything
-                    if (coordinates[x][y].freespace == true) {
-                        maxarm = -2*y+16;
-                        // mvprintw(12,60,"1");
-                    }
-                    if ((coordinates[x][y].freespace == false) && (y == 6)) {
-                        maxarm = 4;
-                        // mvprintw(12,60,"2");
-                    }
-                } else if (!holding.freespace) {// holding a crate
-                    if ((coordinates[x][y].freespace == true) && (y == 0)) {
-                        maxarm = 14;
-                        // mvprintw(12,60,"3");
-                    }
-                    if (coordinates[x][y].freespace == true) {
-                        maxarm = -2*y+13;
-                        // mvprintw(12,60,"4");
-                    }
-                }
-            
-                if ((status == lowering) && (arm < maxarm)) {
-                    arm++;
-                }
-                if (arm == maxarm) {
+                if (arm < 0) {
                     status = raising;
-                    // y = (maxarm-15)/-2;
-                    // y = 2;
+                }
 
+                if (arm == maxarm) { //pick up or place
+                    onclick = -1;
                     if (holding.freespace) { // grabbing
-                        coordinates[x][y].freespace = true;
-                        holding = coordinates[x][y];
-                        columnsize[x]--;
-                        // coordinates[x][y].colour = 0;
+                        if (!(inventory.isEmpty(x))) {
+                            holding = inventory.pop(x);
+                        }
                     } else if (!holding.freespace) { // placing
-                        coordinates[x][y] = holding;
-                        holding.freespace = true;
-                        columnsize[x]++;
+                        isHolding = !(inventory.push(x, holding));
                     }
                 }
-                if ((status == raising) && (arm > 0)) {
-                    arm--;
-                }
-
             }
         }
 
@@ -219,57 +211,7 @@ int ch;
 int currentscore = 0;
 bool gameover = false;
 
-player player;
-
-// different colours
-//COLOR_YELLOW, COLOR_CYAN, COLOR_BLUE, COLOR_WHITE, COLOR_RED, COLOR_GREEN, COLOR_MAGENTA
-
-//functions
-
-void inventoryspawn() {
-    // srand (time(0));
-    int randomcrate = rand() % 100;
-    int randomcoliumn = rand() % 7;
-    string craterarity[100] = {"white", "pink", "pink", "red", "red", "red", "yellow", "yellow", "yellow", "yellow", "green", "green", "green", "green", "green", "green", "green", "green", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "cyan", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing"};
-    if (!(craterarity[randomcrate] == "nothing") && !(player.x == randomcoliumn)) {
-        for (int i = 0; i < 6; i++) {
-            if (coordinates[randomcoliumn][i].freespace == true) {
-                crate newcrate;
-                if (craterarity[randomcrate] == "blue") {
-                    newcrate.colour = 1;//blue
-                } else if (craterarity[randomcrate] == "cyan") {
-                    newcrate.colour = 2;//cyan
-                } else if (craterarity[randomcrate] == "green") {
-                    newcrate.colour = 3;//green
-                } else if (craterarity[randomcrate] == "yellow") {
-                    newcrate.colour = 4;//yellow
-                } else if (craterarity[randomcrate] == "red") {
-                    newcrate.colour = 5;//red
-                } else if (craterarity[randomcrate] == "pink") {
-                    newcrate.colour = 6;//pink
-                } else if (craterarity[randomcrate] == "white") {
-                    newcrate.colour = 7;//white
-                }
-                newcrate.column = randomcoliumn;
-                newcrate.row = i;
-                newcrate.freespace = false;
-                coordinates[randomcoliumn][i] = newcrate;
-                columnsize[randomcoliumn]++;
-                break;
-            }
-        }
-    }
-}
-
-void inventorydraw () {
-    for (int c = 0; c < 7; c++) {
-        for (int r = 0; r < 6; r++) {
-            if (coordinates[c][r].colour != 0 && coordinates[c][r].freespace == false) {
-                coordinates[c][r].draw();
-            }
-        }
-    }
-}
+class player player;
 
 string readLine(string str, int n) {
     // returns the nth line of a string
@@ -291,9 +233,17 @@ void draw() {
     for (int i = 0; i < 18; i++) {
         mvprintw(i,0,readLine(screenstring, i).c_str());
     }
-    inventorydraw();
-    player.draw();
 
+    inventory.draw();
+    player.draw();
+    mvprintw(12,60,"         ");
+    mvprintw(12,60,"%d",inventory.top[player.x]);
+    mvprintw(13,60,"         ");
+    if (!player.holding.freespace) {
+        mvprintw(13,60,"false");
+    } else if (player.holding.freespace) {
+        mvprintw(13,60,"true");
+    }
     attrset(COLOR_PAIR(7)); // delete this line for a cool effect :)
 }
 
@@ -301,48 +251,62 @@ void game() {
 	switch(ch) {
         case '1':
             player.button = 0;
+            player.onclick = 0;
             break;
         case '2': 
             player.button = 1;
+            player.onclick = 1;
             break;
         case '3': 
             player.button = 2;
+            player.onclick = 2;
             break;
         case '4':
             player.button = 3;
+            player.onclick = 3;
             break;
         case '5':
             player.button = 4;
+            player.onclick = 4;
             break;
         case '6':
             player.button = 5;
+            player.onclick = 5;
             break;
         case '7':
             player.button = 6;
+            player.onclick = 6;
             break;
         case KEY_MOUSE: 
             MEVENT event;
             if (getmouse(&event) == OK) {
                 if (event.x >= 0 && event.x <= 8) {
                     player.button = 0;
+                    player.onclick = 0;
                 }
                 if (event.x >= 9 && event.x <= 15) {
                     player.button = 1;
+                    player.onclick = 1;
                 }
                 if (event.x >= 16 && event.x <= 22) {
                     player.button = 2;
+                    player.onclick = 2;
                 }
                 if (event.x >= 23 && event.x <= 29) {
                     player.button = 3;
+                    player.onclick = 3;
                 }
                 if (event.x >= 30 && event.x <= 36) {
                     player.button = 4;
+                    player.onclick = 4;
                 }
                 if (event.x >= 37 && event.x <= 43) {
                     player.button = 5;
+                    player.onclick = 5;
                 }
                 if (event.x >= 44) {
                     player.button = 6;
+                    player.onclick = 6;
                 }
             }
 
@@ -352,7 +316,7 @@ void game() {
     }
 
     player.move();
-    inventoryspawn();
+    inventory.spawn(player.x);
 }
 
 //main game loop
@@ -380,12 +344,11 @@ int main()
     init_pair(8, COLOR_BLACK, -1);
 
     player.status = player::standby;
-    // player.status = player::lowering;
-    // player.status = "grabbing";
+    player.isHolding = false;
     player.holding.freespace = true;
     for (int y = 0; y < 7; y++) {
         for (int x = 0; x < 6; x++) {
-            coordinates[y][x].freespace = true;
+            inventory.coordinates[y][x].freespace = true;
         }
     }
 
